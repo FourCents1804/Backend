056f6a50841bae5e275088fc9ee9582a4659971e
archon21 <rwetmore3@gmail.com>
2018-07-03 10:15:32 -0400

    changed readFileAsync to read File

--- server/api/receiptRecognition.js
+++ server/api/receiptRecognition.js
@@ -1,5 +1,5 @@
 const router = require('express').Router();
-const defaultHandler = require('./errorHandler');
+const defaultHandler = require('../../errorHandler');
 const vision = require('@google-cloud/vision');
 const fs = require('fs')
 const clientVision = new vision.ImageAnnotatorClient({
@@ -13,8 +13,8 @@ const language = require('@google-cloud/language');
 const clientLanguage = new language.LanguageServiceClient({
   projectId: 'dime-app-208122',
   credentials: {
-    private_key: process.env.GOOGLE_PRIVATE_KEY.replace(/\\n/g, '\n'),
-    client_email: process.env.GOOGLE_CLIENT_EMAIL
+    private_key:   process.env.GOOGLE_PRIVATE_KEY.replace(/\\n/g, '\n'),
+    client_email:  process.env.GOOGLE_CLIENT_EMAIL
   }
 });
 
@@ -22,19 +22,19 @@ router.post(
   '/',
   defaultHandler(async (req, res, next) => {
     console.log('In server');
-    // let buff = Buffer.from(req.body.fileName, 'base64')
+    let buff = Buffer.from(req.body.fileName, 'base64')
 
-    // fs.writeFileSync('file-to-send-to-google.jpeg', buff);
+    await fs.writeFile('file-to-send-to-google.jpeg', buff);
     const textOnReceipt = await clientVision.textDetection(
-     req.body.fileName
+      'file-to-send-to-google.jpeg'
 );
-    console.log(textOnReceipt);
+    console.log(textOnReceipt[0].textAnnotations[0].description);
     const elements = textOnReceipt[0].textAnnotations[0].description.split(
       '\n'
     );
     const items = [];
     const prices = [];
-    console.log(elements);
+    console.log(elements)
     // elements.forEach(el => {
     //   if (el === el.toUpperCase() && !el.match(/\d/)) items.push(el)
     //   else if ((el.includes('$') && el.length < 9) || (el.includes('.') && el.length < 9)) prices.push(el)
@@ -49,7 +49,6 @@ router.post(
     });
     const brand = elements[0];
     let summary = {};
-
     if (brand === 'Walmart') {
       const TotalPrice = prices[prices.length - 2];
       const subTotalIdx = items.findIndex(el => {
@@ -67,7 +66,8 @@ router.post(
       summary.Total = TotalPrice;
       summary.purchasedItems = boughtItems;
     }
-    if (brand === 'WHOLE' || brand === 'FOODS') {
+
+    if (brand === 'WHOLE') {
       const NetSalesIdx = items.findIndex(el => {
         return el.startsWith('Net Sales');
       });
@@ -75,7 +75,7 @@ router.post(
         return el.startsWith('Subtotal');
       });
       const TotalPrice = elements[TotalIdx - 1].replace(/\$/g, '');
-      const boughtItems = items.slice(4, NetSalesIdx);
+      const boughtItems = items.slice(5, NetSalesIdx);
       const document = {
         content: boughtItems.join(' '),
         type: 'PLAIN_TEXT'
@@ -86,9 +86,10 @@ router.post(
       summary.category = textContent[0].categories[0].name.slice(1);
       summary.Total = Number(TotalPrice);
       summary.purchasedItems = boughtItems;
-      console.log(summary);
     }
-    res.status(200).send(summary);
+    console.log(items, prices)
+    // res.sendStatus(200)
+    res.send(summary);
   })
 );
 
