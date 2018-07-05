4a7d13690064e64766c5d70d1713430ae8a9bc97
archon21 <rwetmore3@gmail.com>
2018-07-04 20:48:09 -0400

    added barcode recognition

--- server/api/receiptRecognition.js
+++ server/api/receiptRecognition.js
@@ -1,94 +1,92 @@
 const router = require('express').Router();
 const defaultHandler = require('../../errorHandler');
 const vision = require('@google-cloud/vision');
-const fs = require('fs')
 const clientVision = new vision.ImageAnnotatorClient({
   projectId: 'dime-app-208122',
   credentials: {
-    private_key: process.env.GOOGLE_PRIVATE_KEY.replace(/\\n/g, '\n'),
-    client_email: process.env.GOOGLE_CLIENT_EMAIL
+    private_key: process.env.GOOGLE_PRIVATE_KEY.replace(
+      /\\n/g,
+      '\n'
+    ),
+    client_email:  process.env.GOOGLE_CLIENT_EMAIL
   }
 });
 const language = require('@google-cloud/language');
 const clientLanguage = new language.LanguageServiceClient({
   projectId: 'dime-app-208122',
   credentials: {
-    private_key:   process.env.GOOGLE_PRIVATE_KEY.replace(/\\n/g, '\n'),
+    private_key: process.env.GOOGLE_PRIVATE_KEY.replace(
+      /\\n/g,
+      '\n'
+    ),
     client_email:  process.env.GOOGLE_CLIENT_EMAIL
   }
 });
 
-router.post(
+router.get(
   '/',
   defaultHandler(async (req, res, next) => {
     console.log('In server');
-    let buff = Buffer.from(req.body.fileName, 'base64')
-
-    await fs.writeFile('file-to-send-to-google.jpeg', buff());
     const textOnReceipt = await clientVision.textDetection(
-      'file-to-send-to-google.jpeg'
-);
-    console.log(textOnReceipt[0].textAnnotations[0].description);
-    const elements = textOnReceipt[0].textAnnotations[0].description.split(
+      'https://firebasestorage.googleapis.com/v0/b/dime-d9d45.appspot.com/o/imagessend-to-google?alt=media&token=2876f23e-0f64-405e-bb1b-b2ab9333e425'
+    );
+    const allElements = textOnReceipt[0].textAnnotations[0].description.split(
       '\n'
     );
+    let theIndex
+    for (let j = 0; j < allElements.length ; j++) {
+      // console.log(allElements[j])
+      if ((allElements[j]).includes('Total')) {
+        theIndex = j
+        break
+      }
+    }
+    const elements = allElements.slice(0, theIndex);
+    console.log(elements)
     const items = [];
     const prices = [];
-    console.log(elements)
-    // elements.forEach(el => {
-    //   if (el === el.toUpperCase() && !el.match(/\d/)) items.push(el)
-    //   else if ((el.includes('$') && el.length < 9) || (el.includes('.') && el.length < 9)) prices.push(el)
 
-    // })
     elements.forEach(el => {
-      if (el.match(/^[A-Za-z]/i)) {
+      if (
+        el === el.toUpperCase() &&
+        !el.includes('$') &&
+        el !== '' &&
+        isNaN(el) &&
+        el
+
+      ) {
         items.push(el);
-      } else if (el.match(/^[0-9]*[.][0-9]/i)) {
+      } else if (
+        (el.includes('$') && el.length < 9) ||
+        (el.includes('.') && el.length < 9 && !el.includes('b'))
+      ) {
         prices.push(el);
       }
     });
-    const brand = elements[0];
-    let summary = {};
-    if (brand === 'Walmart') {
-      const TotalPrice = prices[prices.length - 2];
-      const subTotalIdx = items.findIndex(el => {
-        return el.startsWith('SUBTOTAL');
-      });
-      const boughtItems = items.slice(6, subTotalIdx);
-      const document = {
-        content: boughtItems.join(' '),
-        type: 'PLAIN_TEXT'
-      };
-      const textContent = await clientLanguage.classifyText({
-        document: document
-      });
-      summary.category = textContent[0].categories[0].name.slice(1);
-      summary.Total = TotalPrice;
-      summary.purchasedItems = boughtItems;
-    }
 
-    if (brand === 'WHOLE') {
-      const NetSalesIdx = items.findIndex(el => {
-        return el.startsWith('Net Sales');
-      });
-      const TotalIdx = elements.findIndex(el => {
-        return el.startsWith('Subtotal');
-      });
-      const TotalPrice = elements[TotalIdx - 1].replace(/\$/g, '');
-      const boughtItems = items.slice(5, NetSalesIdx);
-      const document = {
-        content: boughtItems.join(' '),
-        type: 'PLAIN_TEXT'
-      };
-      const textContent = await clientLanguage.classifyText({
-        document: document
-      });
+    const document = {
+      content: items.join(' '),
+      type: 'PLAIN_TEXT'
+    };
+    const textContent = await clientLanguage.classifyText({
+      document: document
+    });
+    let summary = {};
+    let total = prices[prices.length - 1];
+    let purchasedItems = {};
+    let count = -1;
+    items.forEach(item => {
+      count++;
+      purchasedItems[count] = [item, prices[count]];
+    });
+    summary.purchasedItems = purchasedItems;
+    summary.total = total;
+    if (textContent[0].categories[0]) {
       summary.category = textContent[0].categories[0].name.slice(1);
-      summary.Total = Number(TotalPrice);
-      summary.purchasedItems = boughtItems;
+    } else {
+      summary.category = 'Could Not Retreive Category';
     }
-    console.log(items, prices)
-    // res.sendStatus(200)
+    console.log(summary);
     res.send(summary);
   })
 );
